decc80b78bcc8c152a53f141cf5c82e2f1efe725
Richard Treichl <r.treichl@gmail.com>
2015-10-07 23:43:04 +0200

    CHG: frist implementation of adc driver
    NOTE: not tested yet

--- msp430/src/adc.c
+++ msp430/src/adc.c
@@ -20,19 +20,73 @@
 
 #if DRIVER_MSP430_ADC
 
-uint16_t adc_init()
+uint16_t adc_init(uint16_t *storage)
 {
-	ADC10CTL0 |=	ADC10SHT1
-	ADC10CTL1 |=
-	ADC10AE0 |=
-	//Only for MSP430F22xx
-#ifdef ADC10AE1
-			ADC10AE1 |=
-#endif
-	ADC10DTC0 |=
-	ADC10DTC1 |=
-	ADC10SA |=
+	ADC10CTL0 = ADC_REFERENCE_SOURCE |
+				ADC_SAMPLE_HOLD_TIME |
+				ADC_REFERENCE_SAMPLE_RATE |
+				ADC_REFERENCE_OUTPUT |
+				ADC_REFERENCE_BURST |
+				MSC |
+				ADC_REFERENCE_INTERNAL |
+				ADC10ON |
+				ADC10IE;
+
+
+	ADC10CTL1 |= ADC_SAMPLE_HOLD_SOURCE |
+				ADC_DATA_FORMAT |
+				ADC_CLOCK_DIVIDER |
+				ADC_CLOCK_SOURCE |
+				ADC_CONVERSION_MODE;
+
+	ADC10CTL1 |= (7 << 12);
+
+	ADC10DTC1 = 8;
+
+	ADC10SA = (uint16_t)storage; //should be enough for all 8 channels so far 8x16bit
+
+	ADC10DTC0 = ADC10CT;
+
+	ADC10CTL0 |= ENC |
+				 ADC10SC;
+
 	return 0;
 }
+
+uint16_t adc_create(ADC *adc)
+{
+	if(ADC10AE0 & (1 << adc->port)) {
+		return 0; //TODO return Fault!!
+	}
+
+	adc->result = (uint16_t *)ADC10SA + 2 * adc->port; //reconfig pointer where result is stored from ADC
+
+	ADC10AE0 |= (1 << adc->port);
+
+
+	return 0;
+}
+
+uint16_t adc_delete(ADC *adc)
+{
+	if(!(ADC10AE0 & (1 << adc->port))) {
+		return 0; //TODO return Fault!!
+	}
+
+	ADC10AE0 &= ~(1 << adc->port);
+	return 0;
+}
+
+uint16_t adc_busy()
+{
+	return 0;
+}
+
+#pragma vector=ADC10_VECTOR
+__interrupt void adc(void)
+{
+
+}
+
 
 #endif /*DRIVER_MSP430_ADC*/
